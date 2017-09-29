16f1614e79bda7ed03ba71ac84730cfb4b40b993
Jacob Sieberg <jake.sieberg@mail.nidec.com>
2017-09-29 13:44:24 -0500

    Add linting

---
+++ test/pack1/pack.go
@@ -0,0 +1,7 @@
+package pack1
+
+import "fmt"
+
+func Test(val string) {
+	fmt.Printf("Testing %s", val)
+}
---
+++ test/pack1/pack_test.go
@@ -0,0 +1,7 @@
+package pack1
+
+import "testing"
+
+func TestTest(t *testing.T) {
+	t.Log("Test pass.")
+}
