ea966853001649dffca644274f38c440c721d8fe
Jacob Sieberg <jake.sieberg@mail.nidec.com>
2017-10-06 15:37:09 -0500

    Remove pack1

--- test/pack1/pack.go
+++
@@ -1,8 +0,0 @@
-package pack1
-
-import "fmt"
-
-//Test function
-func Test(val string) {
-	fmt.Printf("Testing %s", val)
-}
--- test/pack1/pack_test.go
+++
@@ -1,7 +0,0 @@
-package pack1
-
-import "testing"
-
-func TestTest(t *testing.T) {
-	Test("Stuff")
-}
