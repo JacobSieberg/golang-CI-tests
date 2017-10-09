af9b1d4f265ff58921ea7020422eaa8e36f664a2
Jacob Sieberg <jake.sieberg@mail.nidec.com>
2017-10-09 11:11:19 -0500

    Fix da tings

--- test/cmd/test/main.go
+++ test/cmd/test/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 const version = "50"
 const url = "https://jacobtestupdate.blob.core.windows.net/updates/"
@@ -8,5 +11,7 @@ const url = "https://jacobtestupdate.blob.core.windows.net/updates/"
 func main() {
 	go waitForUpdates(settings{baseURL: url, checkInterval: time.Second * 5})
 	for {
+		fmt.Printf("Current version: %s\n", version)
+		time.Sleep(time.Second * 2)
 	}
 }
