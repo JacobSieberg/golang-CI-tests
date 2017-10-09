290147769ccb602e59b9ad441bb99a30d13afd7d
Jacob Sieberg <jake.sieberg@mail.nidec.com>
2017-10-09 14:38:57 -0500

    Update application

--- test/cmd/test/main.go
+++ test/cmd/test/main.go
@@ -10,6 +10,7 @@ const url = "https://jacobtestupdate.blob.core.windows.net/updates/"
 
 func main() {
 	go waitForUpdates(settings{baseURL: url, checkInterval: time.Second * 5})
+	fmt.Println("Application started")
 	for {
 		fmt.Printf("Current version: %s\n", version)
 		time.Sleep(time.Second * 2)
--- test/cmd/test/updater.go
+++ test/cmd/test/updater.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"crypto"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"os/exec"
 	"runtime"
 	"time"
+
+	"github.com/inconshreveable/go-update"
 )
 
 type settings struct {
@@ -25,8 +33,34 @@ func waitForUpdates(settings settings) {
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println(sum.name)
-		fmt.Println(sum.sum)
+		currentSum, err := getCurrentSha256Sum()
+		if err != nil {
+			fmt.Println(err)
+		}
+		if currentSum != sum.sum {
+			fmt.Println("Update needed.")
+			file, err := fetchBinary(settings, sum.name)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Println("Downloaded")
+			fmt.Println("Starting update...")
+			updateWithChecksum(file, sum.sum)
+			fmt.Println("Updated")
+			file.Close()
+			fmt.Println("Restarting")
+			cmd := exec.Command(os.Args[0], os.Args[1:]...)
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			err = cmd.Start()
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Println("Restarted")
+
+		}
 		time.Sleep(settings.checkInterval)
 	}
 }
@@ -46,6 +80,11 @@ func fetchCheckSum(settings settings) (sha256Sum, error) {
 	}, nil
 }
 
+func fetchBinary(settings settings, name string) (*os.File, error) {
+	url := fmt.Sprintf("%s/%s", settings.baseURL, name)
+	return download(name, url)
+}
+
 func get(url string) ([]byte, error) {
 	var netClient = &http.Client{
 		Timeout: time.Second * 10,
@@ -57,4 +96,55 @@ func get(url string) ([]byte, error) {
 	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	return body, err
+}
+
+func download(filepath string, url string) (*os.File, error) {
+	file, err := os.Create(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func getCurrentSha256Sum() (string, error) {
+	f, err := os.Open(os.Args[0])
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
+
+func updateWithChecksum(binary io.Reader, hexChecksum string) error {
+	checksum, err := hex.DecodeString(hexChecksum)
+	if err != nil {
+		return err
+	}
+	err = update.Apply(binary, update.Options{
+		Hash:     crypto.SHA256, // this is the default, you don't need to specify it
+		Checksum: checksum,
+	})
+	if err != nil {
+		update.RollbackError(err)
+	}
+	return err
 }
