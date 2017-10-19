108c6852b703d3396606d7a6e1c6a5163066796e
Jacob Sieberg <jake.sieberg@mail.nidec.com>
2017-10-19 12:52:27 -0500

    Test updater

--- test/cmd/test/main.go
+++ test/cmd/test/main.go
@@ -3,28 +3,13 @@ package main
 import (
 	"fmt"
 	"time"
-
-	"github.com/jpillora/overseer"
-	"github.com/jpillora/overseer/fetcher"
 )
 
-const version = "4"
-const url = "https://jacobtestupdate.blob.core.windows.net/updates/test-windows.amd64.exe"
+const version = "1"
 
 func main() {
-	overseer.Run(overseer.Config{
-		Program: start,
-		Fetcher: &fetcher.HTTP{
-			URL:      url,
-			Interval: 1 * time.Second,
-		},
-	})
-}
-
-func start(overseer.State) {
-	fmt.Println("Application started")
 	for {
-		fmt.Printf("Current version: %s\n", version)
-		time.Sleep(time.Second * 2)
+		fmt.Printf("Version %s\n", version)
+		time.Sleep(time.Second * 5)
 	}
 }
