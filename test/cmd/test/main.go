c10934a25887dc31ceba04e3345a077d61e571d2
Jacob Sieberg <jake.sieberg@mail.nidec.com>
2017-10-20 14:45:18 -0500

    Removed exit after min

--- test/cmd/test/main.go
+++ test/cmd/test/main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"time"
 )
 
-const version = "12"
+const version = "13"
 
 func main() {
-	go func() {
-		time.Sleep(time.Minute)
-		os.Exit(0)
-	}()
 	for {
 		fmt.Printf("Version %s\n", version)
 		time.Sleep(time.Second * 5)
