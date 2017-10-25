c84eb030fedbbb0ba2e59b27ca2984735151439a
Jacob Sieberg <jake.sieberg@mail.nidec.com>
2017-10-25 08:53:37 -0500

    Exit on signal

--- test/cmd/test/main.go
+++ test/cmd/test/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
-const version = "14"
+const version = "2"
 
 func main() {
 	for {
 		fmt.Printf("Version %s\n", version)
 		time.Sleep(time.Second * 5)
 	}
+}
+
+func exitOnSig() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT)
+	fmt.Println(<-sigs)
+	os.Exit(0)
 }
